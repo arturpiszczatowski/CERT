b985ee132a0ffe8504696e0c082be5a214378b69
Zevs <piszczatowski.artur@gmail.copm>
2021-02-07 01:11:24 +0100

    added buy&sell to usercontroller

--- src/main/java/com/cryptocurrency/cryptoAPI/controller/UserController.java
+++ src/main/java/com/cryptocurrency/cryptoAPI/controller/UserController.java
@@ -2,6 +2,10 @@ package com.cryptocurrency.cryptoAPI.controller;
 
 import com.cryptocurrency.cryptoAPI.model.User;
 import com.cryptocurrency.cryptoAPI.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +23,17 @@ public class UserController extends CrudController<User> {
     }
 
 
-//    @PostMapping("/buy/{symbol}/{amount}")
-//    public ResponseEntity buyCurrency(@PathVariable String symbol, @PathVariable int amount) {
-//
-//    }
+    @PostMapping("/buy/{symbol}/{amount}")
+    public ResponseEntity buyCurrency(@PathVariable String symbol, @PathVariable int amount) {
+        userService.buy("Anastazjan", symbol, amount);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping("/sell/{symbol}/{amount}")
+    public ResponseEntity sellCurrency(@PathVariable String symbol, @PathVariable int amount) {
+        userService.sell("Szambomen", symbol, amount);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
     @Override
     public Function<User, Map<String, Object>> transformToDTO() {
